18685866dd39de7d5dd37cbfc8a2259282b2ee91
pagarcia22 <pagarciad22@gmail.com>
2025-08-09 12:46:40 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Spa Veterinario',
+  description: 'Created with Paola',
+  generator: 'Paola garcia',
 }
 
 export default function RootLayout({
